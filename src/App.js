253f883d69fd3afc17ace3b99112dc9b2b3da3a3
haris-naveed <107304708+haris-naveed@users.noreply.github.com>
2022-12-27 14:31:05 +0500

    Remove files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment, PureComponent } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App ">
+       abc
+      </div>
+    );
+  }
+}
+
+export default App;
